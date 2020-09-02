1e4a2ce3e9f06a4019d581e9cfdba198f17f46bc
AmarsDing <dingsheng88888888@126.com>
2020-09-02 09:22:01 +0800

    1

---
+++ creator/factory/demo_1_test.go
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-01 22:00:19
+ * @LastEditTime: 2020-09-02 09:21:48
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \golang-GOF\creator\factory\demo_1_test.go
+ */
